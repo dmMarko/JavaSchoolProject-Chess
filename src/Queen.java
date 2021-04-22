b648be274c84c368a01660b809680ea5d5afa733
yoyobara <yotambarak6@gmail.com>
2021-04-22 18:51:09 +0300

    Queen class added

---
+++ src/Queen.java
@@ -0,0 +1,63 @@
+public class Queen extends Piece {
+
+    protected Queen(int tag, Board state) {
+        super(tag, state);
+        symbol = 'q';
+    }
+
+    @Override
+    public int[][] getValidSpots(int[] spot) {
+        Piece[][] board = this.state.getRawBoard();
+        int[][] spots = new int[27][2];
+        int[] checkedSpot;
+
+        int indexCounter = 0;
+
+        // rook-like
+        for (int axis : Utilities.AXIS_ITER) {
+            for (int d : Utilities.PLUS_MINUS) {
+                boolean breakCondition = true;
+                int counter = 1;
+                while (breakCondition) {
+                    if (axis == 0) {
+                        checkedSpot = new int[] { spot[0], spot[1] + counter * d };
+                    } else {
+                        checkedSpot = new int[] { spot[0] + counter * d, spot[1] };
+                    }
+                    if (board[checkedSpot[0]][checkedSpot[1]].getTag() == EMPTY) {
+                        spots[indexCounter++] = checkedSpot;
+                    } else if (board[checkedSpot[0]][checkedSpot[1]].getTag() == -this.tag) {
+                        spots[indexCounter++] = checkedSpot;
+                        breakCondition = false;
+                    } else {
+                        breakCondition = false;
+                    }
+                }
+            }
+        }
+
+        // bishop-like
+        for (int hd : Utilities.PLUS_MINUS) {
+            for (int vd : Utilities.PLUS_MINUS) {
+                try {
+                    for (int i = 1; true; i++) {
+                        checkedSpot = new int[] { spot[0] + i * vd, spot[1] + i * hd };
+                        if (board[checkedSpot[0]][checkedSpot[1]].getTag() == EMPTY) {
+                            spots[indexCounter++] = checkedSpot;
+                        } else if (board[checkedSpot[0]][checkedSpot[1]].getTag() == -this.tag) {
+                            spots[indexCounter++] = checkedSpot;
+                            throw new IndexOutOfBoundsException("reached a piece of opposite colour");
+                        } else {
+                            throw new IndexOutOfBoundsException("reached a piece of the same colour");
+                        }
+                    }
+                } catch (IndexOutOfBoundsException e) {
+                }
+            }
+        }
+
+        return spots;
+
+    }
+
+}
