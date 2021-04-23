5f68b9c8c7ecd9bd65334f4396445eb226f61dc3
yoyobara <yotambarak6@gmail.com>
2021-04-23 17:51:00 +0300

    comments for rook and bishop

--- src/Bishop.java
+++ src/Bishop.java
@@ -1,36 +1,47 @@
-public class Bishop extends Piece{
+public class Bishop extends Piece {
 
     protected Bishop(int tag, Board state) {
         super(tag, state);
         this.symbol = 'b';
     }
 
-    @Override
+    /**
+     * this method calculates the bishop's leagal moves
+     * 
+     * @param spot - the bishop's position
+     * @return all leagal spots the bishop can move to
+     */
     public int[][] getValidSpots(int[] spot) {
         Piece[][] board = this.state.getRawBoard();
         int[][] spots = new int[13][2];
         int[] checkedSpot;
-        
+
         int indexCounter = 0;
-        for (int hd : Utilities.PLUS_MINUS){
-            for (int vd : Utilities.PLUS_MINUS){
-                try{
-                    for(int i = 1; true; i++){
-                        checkedSpot = new int[]{spot[0] + i*vd, spot[1] + i*hd};
-                        if(board[checkedSpot[0]][checkedSpot[1]].getTag() == EMPTY){
-                            spots[indexCounter++] = checkedSpot;
-                        } else if(board[checkedSpot[0]][checkedSpot[1]].getTag() == -this.tag) {
-                            spots[indexCounter++] = checkedSpot;
-                            throw new IndexOutOfBoundsException("reached a piece of opposite colour");
+
+        // checking 4 diagonal directions
+        for (int hd : Utilities.PLUS_MINUS) { // horizontal direction
+            for (int vd : Utilities.PLUS_MINUS) { // vertical direction
+                try {
+                    for (int i = 1; true; i++) {
+                        checkedSpot = new int[] { spot[0] + i * vd, spot[1] + i * hd }; // currently the checked spot
+                        if (board[checkedSpot[0]][checkedSpot[1]].getTag() == EMPTY) { // if empty spot
+                            spots[indexCounter++] = checkedSpot; // add to available spots
+                        } else if (board[checkedSpot[0]][checkedSpot[1]].getTag() == -this.tag) { // if spots is foe
+                            spots[indexCounter++] = checkedSpot; // add to available spots
+                            throw new IndexOutOfBoundsException("reached a piece of opposite colour"); // break from the
+                                                                                                       // current
+                                                                                                       // diagonal
                         } else {
-                            throw new IndexOutOfBoundsException("reached a piece of the same colour");
+                            throw new IndexOutOfBoundsException("reached a piece of the same colour"); // break from the
+                                                                                                       // current
+                                                                                                       // diagonal
                         }
                     }
-                } catch (IndexOutOfBoundsException e){
+                } catch (IndexOutOfBoundsException e) { // if checked spot out of bounds just skip it
                 }
             }
         }
         return spots;
     }
-    
+
 }
--- src/Rook.java
+++ src/Rook.java
@@ -5,39 +5,49 @@ public class Rook extends Piece {
         this.symbol = 'r';
     }
 
-    @Override
+    /**
+     * this method calculates the rook's leagal moves
+     * 
+     * @param spot - the rook's position
+     * @return all leagal spots the rook can move to
+     */
     public int[][] getValidSpots(int[] spot) {
         Piece[][] board = this.state.getRawBoard();
         int[][] spots = new int[14][2];
         int[] checkedSpot;
 
+        // checking both direction for each axis
         int indexCounter = 0;
-        for (int axis : Utilities.AXIS_ITER) {
-            for (int d : Utilities.PLUS_MINUS) {
+        for (int axis : Utilities.AXIS_ITER) { // x axis, y axis
+            for (int d : Utilities.PLUS_MINUS) { // negative direction, positive direction
                 boolean breakCondition = true;
-                int counter = 1;
-                while(breakCondition){
-                    try{
-                        if(axis == 0){
-                            checkedSpot = new int[]{spot[0], spot[1]+counter*d};
-                        } else {
-                            checkedSpot = new int[]{spot[0]+counter*d, spot[1]};
+                int counter = 1; // index of the checked spot
+                while (breakCondition) {
+                    try {
+                        if (axis == 0) { // check the correct spot for horizontal axis
+                            checkedSpot = new int[] { spot[0], spot[1] + counter * d };
+                        } else { // check the correct spot for vertical axis
+                            checkedSpot = new int[] { spot[0] + counter * d, spot[1] };
                         }
-                        if(board[checkedSpot[0]][checkedSpot[1]].getTag() == EMPTY){
-                            spots[indexCounter++] = checkedSpot;
-                        } else if(board[checkedSpot[0]][checkedSpot[1]].getTag() == -this.tag){
-                            spots[indexCounter++] = checkedSpot;
-                            breakCondition = false;
-                        } else {
-                            breakCondition = false;
+
+                        if (board[checkedSpot[0]][checkedSpot[1]].getTag() == EMPTY) { // if checked spot is empty
+                            spots[indexCounter++] = checkedSpot; // add spot to available spots
+
+                        } else if (board[checkedSpot[0]][checkedSpot[1]].getTag() == -this.tag) { // if checked spot is
+                                                                                                  // foe
+                            spots[indexCounter++] = checkedSpot; // add spot to avaliable spots
+                            breakCondition = false; // break from the loop
+
+                        } else { // if checked spot is the same color as this piece.
+                            breakCondition = false; // break from the loop
                         }
-                    }catch(IndexOutOfBoundsException e){
+                    } catch (IndexOutOfBoundsException e) { // if checked spot out of bounds break
                         breakCondition = false;
                     }
                     counter++;
                 }
             }
-        } 
+        }
         return spots;
     }
 
