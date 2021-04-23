1f1b2d11468123c43454b42a288c504e9ca678ec
gitSnes <markovitsdan@gmail.com>
2021-04-24 00:56:45 +0300

    added comments and fixed InputManager

--- src/InputManager.java
+++ src/InputManager.java
@@ -1,23 +1,34 @@
 import java.util.regex.*;
 
 public class InputManager {
-    private static Pattern parsePattern = Pattern.compile("\\W*([a-hA-H])([1-8])\\W*([a-hA-H])([1-8])\\W*");
+    private static Pattern parsePattern = Pattern.compile("([a-hA-H])([1-8])\\W([a-hA-H])([1-8])\\W*"); // regex pattern
 
-    private static int[] convertChessCoords(char file, int rank) {
-        int col = file - 'a';
-        int row = 8 - rank;
+    /**
+     * this method converts a universal chess coordinate to the program's coordinates
+     * @param file - universal chess coordinate file (row)
+     * @param rank - universal chess coordinate rank (column)
+     * @return the location as the program's coordinates
+     * 
+     * note that the method gets the input as column-row(intuative for humans) but returns as row-column(how the program works)
+     */
+    public static int[] convertChessCoords(char file, int rank) { 
+        int col = Character.toLowerCase(file) - 'a'; // get the file/row from a-h to 0-8
+        int row = 8 - rank; // get the rank/column from 8-0 to 0-8
 
-        return new int[] { col, row };
+        return new int[] {row, col};
     }
 
+    /**
+     * this methd extracts the coords from the user's input
+     */
     public static int[][] parseInput(String input) {
-        Matcher inputMatcher = parsePattern.matcher(input);
+        Matcher inputMatcher = parsePattern.matcher(input); // match the input with the pattern
 
-        if (!inputMatcher.matches()) {
+        if (!inputMatcher.matches()) { // if the input doesn't match the pattern, we can't extract the coords
             return null;
         }
 
-        return new int[][] {
+        return new int[][] { // if it does, extract the coords
                 convertChessCoords(inputMatcher.group(1).charAt(0), Integer.parseInt(inputMatcher.group(2))),
                 convertChessCoords(inputMatcher.group(3).charAt(0), Integer.parseInt(inputMatcher.group(4))) };
     }
--- src/testApp.java
+++ src/testApp.java
@@ -1,13 +1,15 @@
 class testApp {
     public static void main(String[] args) {
-        Board game = new Board();
-        System.out.println(game);
-        if(game.canMoveFromTo(new int[]{7,1}, new int[]{0,0})){
-            game.movePieceFromTo(new int[]{7,1}, new int[]{0,0});
-        }
-        if(game.canMoveFromTo(new int[]{7,0}, new int[]{0,0})){
-            game.movePieceFromTo(new int[]{7,0}, new int[]{0,0});
-        }
-        System.out.println(game);
+        int[][] x = InputManager.parseInput("a3-b4 ");
+        System.out.println("" + x[0][0] + "," + x[0][1] + "   " + x[1][0] + "," + x[1][1]);
+        x = InputManager.parseInput("a8-a1 ");
+        System.out.println("" + x[0][0] + "," + x[0][1] + "   " + x[1][0] + "," + x[1][1]);
+        x = InputManager.parseInput("h1-h8 ");
+        System.out.println("" + x[0][0] + "," + x[0][1] + "   " + x[1][0] + "," + x[1][1]);
+        x = InputManager.parseInput("e4-d5 ");
+        System.out.println("" + x[0][0] + "," + x[0][1] + "   " + x[1][0] + "," + x[1][1]);
+
+        int[] y = InputManager.convertChessCoords('a', 1);
+        System.out.println(y[0] + "," + y[1]);
     }
 }
