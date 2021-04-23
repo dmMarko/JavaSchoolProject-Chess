9ab19d4f8087f87b05034b36d6bc7f9ecfd64ef4
gitSnes <markovitsdan@gmail.com>
2021-04-23 17:41:39 +0300

    added main menu(in App class) and added comments to App and Utilities

--- src/App.java
+++ src/App.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args){        
+    public static void main(String[] args){
+        Scanner inputGetter = new Scanner(System.in); // input
+        String input = ""; // the string that saves the user's input
+
+        while(!input.equals("play")){ // the game only starts when the user wants to play
+            System.out.println("Welcom to chess for 2 \ntype “play” or “rules”"); // welcome message
+            input = inputGetter.nextLine().toLowerCase(); // get the input
+            while(!(input.equals("play") || input.equals("rules"))){ // try again if the input wasn't valid
+                System.out.println("Invalid input, please try again"); // fail message
+                input = inputGetter.nextLine().toLowerCase(); // get the input
+            }
+            if(input.equals("rules")){ // if the user wanted the rules, give him the rules
+                System.out.println(Utilities.RULES_TEXT); // print the rules
+            }
+        }
     }
 }
