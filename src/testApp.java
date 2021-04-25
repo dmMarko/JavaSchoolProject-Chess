e16e7ca04c44a19c109ed19308c4f69420bacc2f
yoyobara <yotambarak6@gmail.com>
2021-04-25 09:38:45 +0300

    began working on graphics

--- src/testApp.java
+++ src/testApp.java
@@ -1,5 +1,30 @@
-class testApp {
-    public static void main(String[] args) {
+import java.awt.Graphics;
+import java.awt.*;
+import javax.swing.*;
 
+class testApp extends JPanel {
+
+    public static final Image boardImg = new ImageIcon("resources/Board.png").getImage();
+
+    public testApp() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Toolkit t = Toolkit.getDefaultToolkit();
+        Image i = t.getImage("src/resources/Board.png");
+        g.drawImage(i, 0, 0, this);
+    }
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Chess");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(800, 800);
+        testApp panel = new testApp();
+        window.add(panel);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
